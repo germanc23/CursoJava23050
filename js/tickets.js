3aa8afd222e3de4caf9294f5c1403782cc7a7df6
germanc23 <germancantarella@gmail.com>
2023-05-27 19:05:18 -0300

    act JS

--- js/tickets.js
+++ js/tickets.js
@@ -7,3 +7,28 @@ alert ("usted debe salir del sitio");
 } else {
 alert ("puede ingresar al sitio");
 }
+
+let precio = parseInt(document.getElementById("precio").innerText);
+let btnBorrar = document.getElementById("borrar");
+let btnResumen = document.getElementById("resumen");
+
+function resumen(precio, cantidad, categoria) {
+  let total = precio * cantidad;
+  let descuento = (total * categoria) / 100;
+  return total - descuento;
+}
+
+btnResumen.addEventListener("click", () => {
+  let cantidad = parseInt(document.getElementById("cantidad").value);
+  let categoria = parseInt(document.getElementById("categoria").value);
+  document.getElementById("precioPagar").innerText = resumen(
+    precio,
+    cantidad,
+    categoria
+  );
+});
+
+btnBorrar.addEventListener("click", () => {
+  document.getElementById("cantidad").value = 0;
+  document.getElementById("precioPagar").innerText = 0;
+});
